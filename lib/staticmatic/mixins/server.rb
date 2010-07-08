5f0fee0cd49b8300a1c402f9a2e8497710688d9d
ancu <ancu@ancu.(none)>
2010-07-08 13:00:43 +0300

    optional -b parameter to preview

--- lib/staticmatic/mixins/server.rb
+++ lib/staticmatic/mixins/server.rb
@@ -1,6 +1,15 @@
 module StaticMatic::ServerMixin
   def preview
+    directory = ARGV[1]
+    optional_build = ARGV[2]
+    staticmatic = StaticMatic::Base.new(directory)
+    
+    #building if third argument is -b
+    if optional_build == '-b'
+      staticmatic.run('build')
+    end
+    
     puts "StaticMatic Preview Server Starting..."
-    StaticMatic::Server.start(self)
+    StaticMatic::Server.start(self)   
   end
 end
