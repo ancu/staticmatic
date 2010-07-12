5e6268c1696e4fb72c7b5afbd707617e9296ce9b
ancu <ancu@ancu.(none)>
2010-07-12 15:23:53 +0300

    sdf

--- lib/staticmatic/mixins/server.rb
+++ lib/staticmatic/mixins/server.rb
@@ -1,14 +1,5 @@
 module StaticMatic::ServerMixin
-  def preview
-    directory = ARGV[1]
-    optional_build = ARGV[2]
-    staticmatic = StaticMatic::Base.new(directory)
-    
-    #building if third argument is -b
-    if optional_build == '-b'
-      staticmatic.run('build')
-    end
-    
+  def preview   
     puts "StaticMatic Preview Server Starting..."
     StaticMatic::Server.start(self)   
   end
